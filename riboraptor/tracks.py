9df19ac6e6769951fd1af13f6be1b079b5919c30
Saket Choudhary <saketkc@gmail.com>
2018-09-06 18:39:27 -0700

    Automate track creation

--- riboraptor/tracks.py
+++ riboraptor/tracks.py
@@ -1,9 +1,11 @@
 import glob
 import os
-import inspect
+from textwrap import indent, dedent
+import textwrap
 import sys
 import trackhub
 
+INDENT_SPACES = '\t'
 
 def create_track(hub_name,
                  short_label,
@@ -41,14 +43,16 @@ def create_track(hub_name,
 
 
 def get_compositetrack_text(track_name, parent):
-    header = """track {0}
+    header = """\
+    track {0}
     compositeTrack on
     group {0}
     parent {1}
     shortLabel {0}
     longLabel {0}
-    visibility full\n""".format(track_name, parent)
-    return inspect.cleandoc(header)
+    visibility full
+    \n""".format(track_name, parent)
+    return dedent(header)
 
 
 def get_multiwigtrack_text(track_name, parent):
@@ -73,7 +77,8 @@ def get_multiwigtrack_text(track_name, parent):
         color 120,235,204
 
     """
-    header = """track {0}
+    header = """\
+    track {0}
     container multiWig
     parent {1}
     aggregate transparentOverlay
@@ -83,21 +88,24 @@ def get_multiwigtrack_text(track_name, parent):
     shortLabel {0}
     longLabel {0}
     \n""".format(track_name, parent)
-    return header
+    return dedent(header)
 
 
 def get_supertrack_text(track_name):
-    header = """track {0}
+    header = """\
+    track {0}
     superTrack on
     group {0}
     shortLabel {0}
     longLabel {0}
-    visibility full\n""".format(track_name)
-    return inspect.cleandoc(header)
+    visibility full
+    \n""".format(track_name)
+    return dedent(header)
 
 
 def get_bigwigtrack_text(track_name, parent, big_data_url):
-    text = """track {0}
+    text = """\
+    track {0}
     parent {1}
     priority 300
     longLabel {0}
@@ -109,9 +117,9 @@ def get_bigwigtrack_text(track_name, parent, big_data_url):
     gridDefault on
     color 24,90,197
     visibility full
-    bigDataUrl {2}\n""".format(track_name, parent, big_data_url)
-    text = inspect.cleandoc(text)
-    return text
+    bigDataUrl {2}
+    \n""".format(track_name, parent, big_data_url)
+    return dedent(text)
 
 
 def create_trackdb(bwdir, srp):
@@ -136,30 +144,39 @@ def create_trackdb(bwdir, srp):
     # Step 0. Create super track
 
     srp_header = get_supertrack_text(srp)
-    master_text = srp_header
+    master_text = '###########################################\n'+srp_header
     # Step 1. Create composite track for SRX
     for srx in os.listdir(bwdir):
-        srx_header = get_compositetrack_text(srx, srp)
+
+        master_text += '\n\n###############SRXHeader begin####################\n\n'
+        srx_header = indent(get_compositetrack_text(srx, srp), INDENT_SPACES)
+        master_text += srx_header
         # Step 2. Inside each composite track for a SRX, create another
         # composite track for different orientations/strand inside which
         # we need another multiwig track comprising all fragments for this
         # particular orientation/strand
         for orientation_strand in bw_types:
-            orientation_header = get_compositetrack_text(
-                orientation_strand, srx)
-            multiwig_header = get_multiwigtrack_text(
-                orientation_strand + 'multiWig', orientation_strand)
+            orientation_header = indent(get_compositetrack_text(
+                srx + '_' + orientation_strand, srx), 2*INDENT_SPACES)
+            multiwig_header = indent(get_multiwigtrack_text(
+                srx+ '_' + orientation_strand + '_multiWig', srx + '_' + orientation_strand), 3*INDENT_SPACES)
             bigwig_text = ''
-            for read_length in os.listdir(os.path.join(bwdir, srx)):
+            for read_length in sorted(os.listdir(os.path.join(bwdir, srx))):
                 bwpath = os.path.join(bwdir, srx, read_length,
                                       orientation_strand + '.bw')
+                if not os.path.isfile(bwpath):
+                    sys.stderr.write(bwpath+'\n')
+                    continue
+                if os.stat(bwpath) == 0:
+                    continue
                 track_name = '{}_{}_{}'.format(srx, read_length,
                                                orientation_strand)
                 # Step 2a: Create multiwig track
-                bigwig_text += get_bigwigtrack_text(
-                    track_name, orientation_strand + 'multiWig', bwpath)
-
-        master_text += srx_header + '\n' + orientation_header + '\n' + multiwig_header + '\n' + bigwig_text
+                bwpath = bwpath.replace('/staging/as/skchoudh/re-ribo-analysis/', 'http://smithlab.usc.edu/lab/public/skchoudh/riboraptor_trackhub/hg38/').replace('mapped/', '')
+                bigwig_text += indent(get_bigwigtrack_text(
+                    track_name, srx + '_' + orientation_strand + '_multiWig', bwpath), 4*INDENT_SPACES)
+            master_text += '\n\n' + orientation_header + '\n\n' + multiwig_header + '\n\n' + bigwig_text
+        master_text += '\n\n###############SRXHeader end####################'
     return master_text
 
 
