a2fc3395c2d0a998075b2d7ddfd2af8d1a74d7ca
Saket Choudhary <saketkc@gmail.com>
2019-12-10 14:14:37 -0800

    Import some methods to global namespace

--- riboraptor/__init__.py
+++ riboraptor/__init__.py
@@ -1,3 +1,6 @@
 __author__ = """Saket Choudhary, Wenzheng Li"""
 __email__ = "saketkc@gmail.com"
 __version__ = "0.4.0"
+
+from .GTF import gtf_to_dataframe
+from .plot_talk_theme import load_pub_theme
